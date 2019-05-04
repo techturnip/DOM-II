1f2ce0a2a47a1675fbab32a874a2adbcaf4ba1d5
Tyler Turnipseed <tyler@techturnip.us>
2019-05-04 14:23:53 -0400

    Added forms to .destination elements, added random color function, used stopPropagation to prevent the color change on the nav links, added 'mouse move' event listener, added 'focus' event handler to navLinks which console.logs the innerText, added 'scroll' event listener that changes the background on scroll, added keydown event to the input elements, added cut and paste events to the input elements

--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
-/* #1 On Click Event */
-const nav = document.querySelector('.nav').addEventListener('click', onClick);
-
-function onClick(e) {
+function randColor() {
   let char = '0123456789ABCDEF';
   let color = '#';
   for (let i = 0; i < 6; i++) {
     color += char[Math.floor(Math.random() * 16)];
   }
 
-  e.target.style.color = color;
+  return color;
 }
+
+/* #1 On Click Event */
+const nav = document.querySelector('.nav');
+const navLink = document.querySelectorAll('.nav-link');
+
+nav.addEventListener('click', onClick);
+
+function onClick(e) {
+  e.target.style.color = randColor();
+
+  e.preventDefault();
+}
+
+navLink.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.stopPropagation();
+    console.log('link clicked');
+  });
+});
 
 /* #2 On Load Event */
 const funBusImg = document
@@ -41,9 +57,83 @@ function onMouseOver(e) {
 }
 
 /* #5 Mouse Move event */
-const body = document
-  .querySelector('body')
-  .addEventListener('mousemove', function(e) {
-    console.log('Screen position X: ' + e.offsetX);
-    console.log('Screen position Y: ' + e.offsetY);
+const offset = document.createElement('a');
+
+offset.textContent = 'Offset';
+
+console.log(nav);
+
+nav.appendChild(offset);
+
+const body = document.querySelector('body');
+
+body.addEventListener('mousemove', e => {
+  offset.textContent = `X: ${e.offsetX} Y: ${e.offsetY}`;
+});
+
+/* #6 On Focus Event */
+
+navLink.forEach(link => {
+  link.addEventListener('focus', onFocus);
+});
+
+function onFocus(e) {
+  console.log(`${e.target.innerText} link is in focus`);
+}
+
+/* #7 On Scroll Event */
+const doc = document.documentElement;
+window.addEventListener('scroll', onScroll);
+let randBackgroundArr = [
+  randColor(),
+  randColor(),
+  randColor(),
+  randColor(),
+  randColor(),
+  randColor()
+];
+
+function onScroll(e) {
+  let top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+
+  if (top > 0 && top < 100) {
+    e.target.bgColor = randBackgroundArr[0];
+  } else if (top > 100 && top < 200) {
+    e.target.bgColor = randBackgroundArr[1];
+  } else if (top > 200 && top < 300) {
+    e.target.bgColor = randBackgroundArr[2];
+  } else if (top > 300 && top < 400) {
+    e.target.bgColor = randBackgroundArr[3];
+  } else if (top > 400 && top < 500) {
+    e.target.bgColor = randBackgroundArr[4];
+  } else if (top > 500 && top < 600) {
+    e.target.bgColor = randBackgroundArr[5];
+  }
+}
+
+/* #8 Keydown Event */
+const destination = document.querySelectorAll('.destination');
+
+destination.forEach(dest => {
+  dest.addEventListener('keydown', keyDown);
+});
+
+function keyDown(e) {
+  e.target.style.border = `solid 1px ${randColor()}`;
+}
+
+/* #9 Cut Event */
+const input = document.querySelectorAll('form input');
+
+input.forEach(inp => {
+  inp.addEventListener('cut', function(e) {
+    alert(`You cut ${e.target.value} into the clipboard`);
   });
+});
+
+/* #10 Paste Event */
+input.forEach(inp => {
+  inp.addEventListener('paste', function(e) {
+    alert(`You pasted into input`);
+  });
+});
